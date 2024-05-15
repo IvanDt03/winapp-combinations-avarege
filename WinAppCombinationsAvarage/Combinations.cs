45051469f77e2f6e513243467a06889310dd09d9
Ivan Dmitirev <vanya2003569@yandex.ru>
2024-05-15 20:52:49 +0300

    Add method GenerateCombinations

--- WinAppCombinationsAvarage/Combinations.cs
+++ WinAppCombinationsAvarage/Combinations.cs
@@ -13,5 +13,21 @@ namespace WinAppCombinationsAvarage
             combinations = new List<double[]>();
             N = inputData.Length;
         }
+
+        public void GenerateCombinations(double[] inputData, int n, int k, List<double> currentCombination, int start)
+        {
+            if (currentCombination.Count == k)
+            {
+                combinations.Add(currentCombination.ToArray());
+                return;
+            }
+
+            for (int i = start; i < n; i++)
+            {
+                List<double> newCombination = new List<double>(currentCombination);
+                newCombination.Add(inputData[i]);
+                GenerateCombinations(inputData, n, k, newCombination, i + 1);
+            }
+        }
     }
 }
