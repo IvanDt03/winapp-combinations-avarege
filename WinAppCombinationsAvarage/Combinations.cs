c196ccf1cc23b6737ac15a33c40bdb8a3c26cad8
Ivan Dmitirev <vanya2003569@yandex.ru>
2024-05-15 20:48:16 +0300

    Add class Combinations

---
+++ WinAppCombinationsAvarage/Combinations.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace WinAppCombinationsAvarage
+{
+    internal class Combinations
+    {
+        private List<double[]> combinations;
+        public int N { get; private set; }
+        public int K { get; } = 4;
+
+        public Combinations(double[] inputData)
+        {
+            combinations = new List<double[]>();
+            N = inputData.Length;
+        }
+    }
+}
--- WinAppCombinationsAvarage/obj/Debug/net8.0-windows/WinAppCombinationsAvarage.AssemblyInfo.cs
+++ WinAppCombinationsAvarage/obj/Debug/net8.0-windows/WinAppCombinationsAvarage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinAppCombinationsAvarage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1634f9674f2e27f9e6d84735426d88e0e148b20a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinAppCombinationsAvarage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinAppCombinationsAvarage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
