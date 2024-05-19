35f5c0515f1549c7562d1bd3704f830ab3c92b26
Ivan Dmitirev <vanya2003569@yandex.ru>
2024-05-19 17:12:48 +0300

    Add icon for window

--- WinAppCombinationsAvarage/Form1.Designer.cs
+++ WinAppCombinationsAvarage/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             lblImputData = new Label();
             dataAvarage = new DataGridView();
             lblError = new Label();
@@ -45,9 +46,9 @@
             // lblImputData
             // 
             lblImputData.AutoSize = true;
-            lblImputData.Location = new Point(12, 73);
+            lblImputData.Location = new Point(10, 55);
             lblImputData.Name = "lblImputData";
-            lblImputData.Size = new Size(329, 20);
+            lblImputData.Size = new Size(259, 15);
             lblImputData.TabIndex = 1;
             lblImputData.Text = "Enter an array of elements separated by a space:";
             // 
@@ -61,20 +62,21 @@
             dataAvarage.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataAvarage.BackgroundColor = Color.DimGray;
             dataAvarage.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataAvarage.Location = new Point(386, 2);
+            dataAvarage.Location = new Point(338, 2);
+            dataAvarage.Margin = new Padding(3, 2, 3, 2);
             dataAvarage.Name = "dataAvarage";
             dataAvarage.ReadOnly = true;
             dataAvarage.RowHeadersWidth = 51;
-            dataAvarage.Size = new Size(675, 571);
+            dataAvarage.Size = new Size(591, 428);
             dataAvarage.TabIndex = 2;
             // 
             // lblError
             // 
             lblError.AutoSize = true;
             lblError.ForeColor = Color.Red;
-            lblError.Location = new Point(12, 126);
+            lblError.Location = new Point(10, 94);
             lblError.Name = "lblError";
-            lblError.Size = new Size(142, 20);
+            lblError.Size = new Size(114, 15);
             lblError.TabIndex = 3;
             lblError.Text = "Incorrect array input";
             // 
@@ -82,9 +84,10 @@
             // 
             btnCalculate.BackColor = Color.LimeGreen;
             btnCalculate.Cursor = Cursors.Hand;
-            btnCalculate.Location = new Point(249, 129);
+            btnCalculate.Location = new Point(218, 97);
+            btnCalculate.Margin = new Padding(3, 2, 3, 2);
             btnCalculate.Name = "btnCalculate";
-            btnCalculate.Size = new Size(119, 49);
+            btnCalculate.Size = new Size(104, 37);
             btnCalculate.TabIndex = 4;
             btnCalculate.Text = "Calculate";
             btnCalculate.UseVisualStyleBackColor = false;
@@ -93,44 +96,47 @@
             // lblN
             // 
             lblN.AutoSize = true;
-            lblN.Location = new Point(12, 21);
+            lblN.Location = new Point(10, 16);
             lblN.Name = "lblN";
-            lblN.Size = new Size(20, 20);
+            lblN.Size = new Size(16, 15);
             lblN.TabIndex = 5;
             lblN.Text = "N";
             // 
             // lblK
             // 
             lblK.AutoSize = true;
-            lblK.Location = new Point(170, 24);
+            lblK.Location = new Point(149, 18);
             lblK.Name = "lblK";
-            lblK.Size = new Size(18, 20);
+            lblK.Size = new Size(14, 15);
             lblK.TabIndex = 6;
             lblK.Text = "K";
             // 
             // txtBoxN
             // 
             txtBoxN.Cursor = Cursors.Hand;
-            txtBoxN.Location = new Point(38, 18);
+            txtBoxN.Location = new Point(33, 14);
+            txtBoxN.Margin = new Padding(3, 2, 3, 2);
             txtBoxN.Name = "txtBoxN";
-            txtBoxN.Size = new Size(50, 27);
+            txtBoxN.Size = new Size(44, 23);
             txtBoxN.TabIndex = 7;
             // 
             // txtBoxK
             // 
             txtBoxK.Cursor = Cursors.Hand;
-            txtBoxK.Location = new Point(194, 21);
+            txtBoxK.Location = new Point(170, 16);
+            txtBoxK.Margin = new Padding(3, 2, 3, 2);
             txtBoxK.Name = "txtBoxK";
-            txtBoxK.Size = new Size(50, 27);
+            txtBoxK.Size = new Size(44, 23);
             txtBoxK.TabIndex = 8;
             // 
             // btnClear
             // 
             btnClear.BackColor = Color.Firebrick;
             btnClear.Cursor = Cursors.Hand;
-            btnClear.Location = new Point(249, 184);
+            btnClear.Location = new Point(218, 138);
+            btnClear.Margin = new Padding(3, 2, 3, 2);
             btnClear.Name = "btnClear";
-            btnClear.Size = new Size(119, 39);
+            btnClear.Size = new Size(104, 29);
             btnClear.TabIndex = 9;
             btnClear.Text = "Clear";
             btnClear.UseVisualStyleBackColor = false;
@@ -139,9 +145,10 @@
             // txtBoxInputData
             // 
             txtBoxInputData.Cursor = Cursors.Hand;
-            txtBoxInputData.Location = new Point(12, 96);
+            txtBoxInputData.Location = new Point(10, 72);
+            txtBoxInputData.Margin = new Padding(3, 2, 3, 2);
             txtBoxInputData.Name = "txtBoxInputData";
-            txtBoxInputData.Size = new Size(361, 27);
+            txtBoxInputData.Size = new Size(316, 23);
             txtBoxInputData.TabIndex = 10;
             txtBoxInputData.TextChanged += TxtBoxInputData_TextChanged;
             // 
@@ -150,19 +157,19 @@
             author.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             author.AutoSize = true;
             author.Font = new Font("Segoe UI", 6F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            author.Location = new Point(2, 534);
+            author.Location = new Point(2, 400);
             author.Name = "author";
-            author.Size = new Size(184, 36);
+            author.Size = new Size(150, 33);
             author.TabIndex = 11;
             author.Text = "Дмитриев Иван Александрович\r\nФундаментальная и прикладная химия\r\n2 группа";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoScroll = true;
             BackColor = Color.CornflowerBlue;
-            ClientSize = new Size(1061, 576);
+            ClientSize = new Size(930, 432);
             Controls.Add(author);
             Controls.Add(txtBoxInputData);
             Controls.Add(btnClear);
@@ -174,7 +181,9 @@
             Controls.Add(lblError);
             Controls.Add(dataAvarage);
             Controls.Add(lblImputData);
-            MinimumSize = new Size(1079, 608);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 2, 3, 2);
+            MinimumSize = new Size(946, 466);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Calculate Average";
--- WinAppCombinationsAvarage/obj/Debug/net8.0-windows/WinAppCombinationsAvarage.AssemblyInfo.cs
+++ WinAppCombinationsAvarage/obj/Debug/net8.0-windows/WinAppCombinationsAvarage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinAppCombinationsAvarage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c521d8f49e9c18474912b92bd116be07a51540e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aaa661e79f328adc28a284aaf60a75709ccb675")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinAppCombinationsAvarage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinAppCombinationsAvarage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
